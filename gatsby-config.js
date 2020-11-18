02b0ca829c1a8701acba9edea69e505d48d863ec
Yuliya Payevska <y.payevska@gmail.com>
2020-11-18 14:30:43 +0200

    Fixed build

--- gatsby-config.js
+++ gatsby-config.js
@@ -38,23 +38,19 @@ module.exports = {
       options: {
         apiURL: `http://localhost:5000`,
         queryLimit: 1000, // Default to 100
-        contentTypes: [`prices`, `projects`, `blogs`],
+        contentTypes: [`prices`, `projects`, `blogs`, `abouts`],
         singleTypes: [],
       },
     },
     {
-      resolve: `gatsby-plugin-prefetch-google-fonts`,
+      resolve: `gatsby-plugin-google-fonts`,
       options: {
         fonts: [
-          {
-            family: `Roboto`,
-            variants: [`400`, `700`],
-          },
-          {
-            family: `Open Sans`,
-          },
+          `Roboto\:400,700`,
+          `Open Sans`
         ],
-      },
-    },
+        display: 'swap'
+      }
+    }
   ],
 }
